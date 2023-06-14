0a0f31c8c34e4171ba58c37d86b125907d0ceee5
Willy <wolf1986@gmail.com>
2023-06-14 20:52:00 +0300

    Utility - Discover sub classes

--- lgtr_infra/utils.py
+++ lgtr_infra/utils.py
@@ -1,5 +1,9 @@
 import datetime
+import inspect
 import json
+import logging
+import pkgutil
+import types
 import uuid
 from typing import Optional, TypeVar, Type, Iterable, Any, Callable
 
@@ -64,3 +68,36 @@ def short_uuid4():
 
 def htimestamp_uuid():
     return htimestamp() + '_' + short_uuid4()
+
+
+def discover_sub_classes(base_class: Type, package: str | types.ModuleType):
+    """
+        Auto traverse a package and its sub-packages to find all classes that are
+        descendants of a given base class.
+    """
+    descendants: dict[str, Type] = {}
+
+    # Import the package
+    if isinstance(package, str):
+        package = __import__(package_name, fromlist=[""])
+
+    # Traverse the package and its sub-packages
+    packages_to_traverse = [package]
+    for package_current in packages_to_traverse:
+        for _module_loader, name, ispkg in pkgutil.walk_packages(package_current.__path__):
+            try:
+                package_or_module = __import__(f"{package_current.__name__}.{name}", fromlist=[""])
+            except (ModuleNotFoundError, NameError):
+                logging.warning(f"Skipping module {name} in package {package_current.__name__} (Unable to import)")
+                continue
+
+            if ispkg:
+                packages_to_traverse.append(package_or_module)
+            else:
+                # Import the module and get its classes
+                for cls_name, cls in inspect.getmembers(package_or_module, inspect.isclass):
+                    # Check if the class is a descendant of the base class
+                    if issubclass(cls, base_class):
+                        descendants[cls_name] = cls
+
+    return descendants
